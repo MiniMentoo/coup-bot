8124de9125b8fe0509298a750173d74ae73b9097
MiniMentoo <mostafaalkhazaali@gmail.com>
2024-06-24 01:56:05 +0100

    Added template for coup.js
    Command takes user and gives helpful error message if target of coup
    isn't valid or if not enough coins, along with the normal error messages
    if the command is run when it's not your turn.

---
+++ commands/coup/coup.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+const {endTurn} = require('../../turn-utils.js');
+const {cardType, cardEmoji, thinkingTime} = require('../../config.json');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('coup')
+        .setDescription('perform the coup action, if it is your turn')
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('the target of the coup')
+                .setRequired(true)),
+    async execute(interaction) {
+        let reply = 'empty';
+        let deployedButtons = false;
+        const target = interaction.options.getUser('target');
+        if (global.games.has(interaction.guild.id) && (!global.gameInfo.get(interaction.guild.id).length == 0)){
+            let players = global.games.get(interaction.guild.id);
+            let turn = global.turns.get(interaction.guild.id);
+            let hands = global.hands.get(interaction.guild.id);
+            if (interaction.user == players[turn]) {
+                if (players.includes(target) && hands.get(target)[3]) {
+                    if (hands.get(interaction.user)[1] >= 7) {
+
+                    } else {
+                        reply = {content : `A Coup requires 7 coins, and you only have ${hands.get(interaction.user)[1]}`, ephemeral : true};
+                    }
+                } else {
+                    reply = {content : `The target isn't part of this game, try again with someone else.`, ephemeral : true};
+                }
+            } else {
+                reply = {content : `You are not the turn player! It's ${players[turn]} turn right now`, ephemeral : true};
+            }
+        } else {
+            reply = {content : `There either isn't a game in this server, or it hasn't been /start 'ed yet`, ephemeral : true};
+        }
+
+        await interaction.reply(reply);
+    }       
+};

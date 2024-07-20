e88dfdf3ecb31bdb9b9c56c17dd2ca31de1ec374
MiniMentoo <mostafaalkhazaali@gmail.com>
2024-07-20 19:14:28 +0100

    Implemented mechanics for captain blocks, and challenging those blocks.

--- commands/coup/steal.js
+++ commands/coup/steal.js
@@ -72,6 +72,23 @@ They will have ${hands.get(interaction.user)[1] + 2} coins if this goes through.
             let players = global.games.get(interaction.guild.id);
             let turn = global.turns.get(interaction.guild.id);
             const collectorFilter = i => players.includes(i.user);
+
+            const challenge = new ButtonBuilder()
+                .setCustomId('challenge')
+                .setLabel('Challenge')
+                .setStyle(ButtonStyle.Danger)
+                .setEmoji('❌');
+
+            const noBlocksEnabled = new ButtonBuilder()
+                .setCustomId('noBlocks')
+                .setLabel('No Blocks / Challenges')
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji('✅')
+                .setDisabled(false);
+
+            let row = new ActionRowBuilder()
+                .addComponents(challenge, noBlocksEnabled);
+
             try {
                 const action = await response.awaitMessageComponent({filter : collectorFilter, time: thinkingTime});
                 await interaction.editReply({components : []});
@@ -80,7 +97,6 @@ They will have ${hands.get(interaction.user)[1] + 2} coins if this goes through.
                     hands.get(target)[1] = hands.get(target)[1] - 2;
                     await action.update({content: `Steal successfully performed! ${players[turn]} has gained 2 coins and now has ${hands.get(players[turn])[1]} coins.
 ${target} has lost 2 coins and now has ${hands.get(target)[1]} coins`, components : []});
-                    await endTurn(action, interaction.guild.id, players);
                 } else if (action.customId == "challenge") {
                     await action.reply(`${action.user} has challenged ${interaction.user}`);
                     if(! await performChallenge(action, action.user, interaction.user, 0)) {
@@ -91,12 +107,38 @@ ${target} has lost 2 coins and now has ${hands.get(target)[1]} coins`, component
                     } else {
                         await action.followUp(`Steal failed as challenge succeeded.`);
                     }
-                    await endTurn(action, interaction.guild.id, players);
+                } else if (action.customId == "captainBlock") {
+                    await action.update({components : []});
+
+                    const challenging = await action.followUp({content : `${action.user} is claiming Captain ${cardEmoji[0]} and has blocked ${interaction.user}'s steal, anyone can challenge this!`, components : [row]})
+                    try {
+                        const choice = await challenging.awaitMessageComponent({ filter: collectorFilter, time: thinkingTime });
+                        if (choice.customId == "noBlocks") {
+                            await choice.reply(`Steal successfully blocked, no challenges`)
+                        } else {
+                            await choice.reply(`${choice.user} has challenged ${action.user}`);
+                            if (await performChallenge(choice, choice.user, action.user, 0)) {
+                                hands.get(players[turn])[1] = hands.get(players[turn])[1] + 2;
+                                hands.get(target)[1] = hands.get(target)[1] - 2;
+                                await choice.followUp({content: `Challenge suceeded, Captain block fails, steal goes through! ${players[turn]} has gained 2 coins and now has ${hands.get(players[turn])[1]} coins.
+${target} has lost 2 coins and now has ${hands.get(target)[1]} coins`, components : []});
+                            } else {
+                                await choice.followUp(`Challenge failed, Captain block remains. Turn passes.`)
+                            }
+                        }
+                    } catch(e) {
+                        console.log(e);
+                        await choice.reply(`Steal successfully blocked, no challenges before timeout`)
+                    }
                 }
+                await endTurn(action, interaction.guild.id, global.games.get(interaction.guild.id));
             } catch(e) {
                 console.log(e);
-                await choice.reply(`Assassination successfully blocked, no challenges before timeout`)
-                endTurn(choice, interaction.guild.id, global.games.get(interaction.guild.id));
+                hands.get(players[turn])[1] = hands.get(players[turn])[1] + 2;
+                hands.get(target)[1] = hands.get(target)[1] - 2;
+                await action.update({content: `Buttons timed out, steal going through, ${players[turn]} has gained 2 coins and now has ${hands.get(players[turn])[1]} coins.
+${target} has lost 2 coins and now has ${hands.get(target)[1]} coins`, components : []});
+                    await endTurn(action, interaction.guild.id, players);
             }
         }
     }       

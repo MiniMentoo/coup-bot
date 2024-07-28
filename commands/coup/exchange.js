06b70bce555fd15323dd46c70fe8bc147db53290
MiniMentoo <mostafaalkhazaali@gmail.com>
2024-07-28 18:53:53 +0100

    Added template response to exchange, began the return two function.

--- commands/coup/exchange.js
+++ commands/coup/exchange.js
@@ -32,7 +32,7 @@ module.exports = {
                 const row = new ActionRowBuilder()
                 .addComponents(challenge, noBlocksEnabled);
 
-                reply = {content : `${interaction.user} is claiming Ambassador ${cardEmoji[2]} is attempting the exchange action
+                reply = {content : `${interaction.user} is claiming Ambassador ${cardEmoji[2]} and attempting the exchange action
 They will draw 2 cards from the deck and pick which roles to keep (they cannot replace revealed cards). This action can only be challenged!`, components : [row]};
             } else {
                 reply = {content : `You are not the turn player! It's ${players[turn]} turn right now`, ephemeral : true};
@@ -41,6 +41,63 @@ They will draw 2 cards from the deck and pick which roles to keep (they cannot r
             reply = {content : `There either isn't a game in this server, or it hasn't been /start 'ed yet`, ephemeral : true};
         }
 
-        await interaction.reply(reply);
+        const response = await interaction.reply(reply);
+        if (deployedButtons){
+            let hands = global.hands.get(interaction.guild.id);
+            let players = global.games.get(interaction.guild.id);
+            let turn = global.turns.get(interaction.guild.id);
+            const collectorFilter = i => players.includes(i.user);
+            const deck = global.gameInfo.get(interaction.guild.id);
+            const hand = hands.get(interaction.user);
+
+            try {
+                const action = await response.awaitMessageComponent({filter : collectorFilter, time: thinkingTime});
+                await interaction.editReply({components : []});
+                if (action.customId == "noBlocks") {
+                    let card1 = deck.splice(0,1);
+                    let card2 = deck.splice(0,1);
+                    if (hand[0][0] == -1) {
+
+                    } else if (hand[0][1] == -1) {
+
+                    } else {
+
+                    }
+                }
+            
+            } catch(e) {
+                console.log(e);
+                await action.update({content: `Buttons timed out, exchange going through`, components : []});
+                await endTurn(action, interaction.guild.id, players);
+            }
+        }
     }       
 };
+
+
+async function returnTwo(card1, card2, card3, interaction) {
+    
+    const return1 = new ButtonBuilder()
+                .setCustomId('return1')
+                .setLabel(`${cardType[card1]}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji(`${cardEmoji[card1]}`);
+
+    const return2 = new ButtonBuilder()
+                .setCustomId('return2')
+                .setLabel(`${cardType[card2]}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji(`${cardEmoji[card2]}`);
+    
+    const return3 = new ButtonBuilder()
+                .setCustomId('return3')
+                .setLabel(`${cardType[card3]}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setEmoji(`${cardEmoji[card3]}`);
+
+    const row = new ActionRowBuilder()
+        .addComponents(return1, return2, return3);
+
+    const response = await interaction.followUp({content : `${interaction.user}, pick TWO of the following cards to return to the deck, the last one will be the card in your hand`, 
+        components : [row], ephemeral : true});
+}
